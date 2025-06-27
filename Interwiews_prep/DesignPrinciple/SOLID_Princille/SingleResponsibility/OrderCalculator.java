1f94165dc0f5a9a1f6c9ae5aec6bd6fc125790f1
Apurv Kumar <apurvsing77@gmail.com>
2025-06-27 21:02:41 +0530

    first commit

---
+++ Interwiews_prep/DesignPrinciple/SOLID_Princille/SingleResponsibility/OrderCalculator.java
@@ -0,0 +1,31 @@
+package Interwiews_prep.DesignPrinciple.SOLID_Princille.SingleResponsibility;
+
+public class OrderCalculator {
+
+    int amount;
+    int discount;
+    int tax;
+
+    public static void main(String[] args) {
+        OrderCalculator oc = new OrderCalculator();
+        oc.amount=5000;
+        oc.discount=25;
+        oc.tax=10;
+
+        oc.orderAmount();
+        EmailSend.sendMail();
+
+    }
+
+    private void orderAmount() {
+
+        int order_amount = amount - (amount*discount)/100 - (amount*tax)/100;
+        System.out.println(order_amount);
+    }
+}
+
+class EmailSend{
+    public static void sendMail(){
+        System.out.println("We have Receive mail of order");
+    }
+}
