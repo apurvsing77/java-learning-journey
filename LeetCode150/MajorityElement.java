c2ddfbc52321211c10819086a868dfcaa3655b3d
Apurv Kumar <apurvsing77@gmail.com>
2025-06-30 23:06:48 +0530

    commit

--- LeetCode150/MajorityElement.java
+++ LeetCode150/MajorityElement.java
@@ -13,8 +13,8 @@ public class MajorityElement {
     public static int majorityElement(int[] nums) {
 
         Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length ; i++) {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
 
         }
 
